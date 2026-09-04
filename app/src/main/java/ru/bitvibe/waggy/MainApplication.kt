9ce5ffe94568b142cb135da366ff81e7a88cd5a3
bitvibe <sterletivakiril@gmail.com>
2026-09-04 18:08:19 +0400

    refactor: move to DI injection workManager and remove useless initialization
    Signed-off-by: bitvibe <sterletivakiril@gmail.com>

--- app/src/main/java/ru/bitvibe/waggy/MainApplication.kt
+++ app/src/main/java/ru/bitvibe/waggy/MainApplication.kt
@@ -5,35 +5,24 @@ import android.util.Log
 import androidx.hilt.work.HiltWorkerFactory
 import androidx.work.Configuration
 import com.google.firebase.Firebase
-import com.google.firebase.analytics.analytics
 import com.google.firebase.crashlytics.crashlytics
-import com.google.firebase.initialize
-import dagger.hilt.EntryPoint
-import dagger.hilt.EntryPoints
-import dagger.hilt.InstallIn
 import dagger.hilt.android.HiltAndroidApp
-import dagger.hilt.components.SingletonComponent
+import javax.inject.Inject
 
 @HiltAndroidApp
 class MainApplication : Application(), Configuration.Provider {
 
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
     override fun onCreate() {
         super.onCreate()
-        Firebase.initialize(this)
-        Firebase.analytics.setAnalyticsCollectionEnabled(true)
-        Firebase.crashlytics.isCrashlyticsCollectionEnabled = true
+        Firebase.crashlytics.isCrashlyticsCollectionEnabled = !BuildConfig.DEBUG
     }
 
-    @EntryPoint
-    @InstallIn(SingletonComponent::class)
-    interface HiltWorkerFactoryEntryPoint {
-        fun workerFactory(): HiltWorkerFactory
-    }
-
-    override val workManagerConfiguration = Configuration.Builder()
-        .setWorkerFactory(
-            EntryPoints.get(this, HiltWorkerFactoryEntryPoint::class.java).workerFactory()
-        )
-        .setMinimumLoggingLevel(Log.INFO)
-        .build()
+    override val workManagerConfiguration: Configuration
+        get() = Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .setMinimumLoggingLevel(if (BuildConfig.DEBUG) Log.DEBUG else Log.ERROR)
+            .build()
 }
